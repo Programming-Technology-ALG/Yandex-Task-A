60c9ff3f3901e0f1296d6fd2ef7404844b9f60b9
Alexey Trutnev <66214960+alexgiving@users.noreply.github.com>
2021-04-26 18:13:41 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,22 +2,16 @@
 
 int main() {
   int n = 0;
-  int answer = 0;
   std::cin >> n;
   int arr[n];
-
-  for (size_t i = 0; i < n; ++i) {
+  std::cin >> arr[0];
+  for (size_t i = 1; i < n; ++i) {
     std::cin >> arr[i];
-  }
-  for (size_t i = 0; i < n - 1; ++i) {
-    if (arr[i] > arr[i + 1]) {
-      answer = -1;
-      break;
+    if (arr[i-1] > arr[i]) {
+      std::cout << -1;
+      return 0;
     }
   }
-  if (answer == 0) {
-    answer = arr[n - 1] - arr[0];
-  }
-  std::cout << answer;
+  std::cout << arr[n - 1] - arr[0];
   return 0;
 }
